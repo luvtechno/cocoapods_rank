482e2b9886716354360f593a827f92d201d00a51
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-06 20:27:57 +0900

    Sort specs by github star count

--- cocoapods_rank.rb
+++ cocoapods_rank.rb
@@ -3,6 +3,12 @@ require 'octokit'
 require 'pry'
 
 class Pod::Specification
+  class << self
+    def github_client
+      client = Octokit::Client.new(oauth_token: ENV['GITHUB_TOKEN'])
+    end
+  end
+
   def git_repo
     source[:git]
   end
@@ -11,16 +17,36 @@ class Pod::Specification
     git_repo && git_repo.start_with?('https://github.com')
   end
 
-  def github_repo
+  def github_repo_name
     match_data = git_repo.match("https://github.com/(.+?)/(.+?).git")
     "#{match_data[1]}/#{match_data[2]}"
   end
 
-
+  def github_repo
+    @repo ||= self.class.github_client.repo(github_repo_name)
+  end
 end
 
 specs = Pod::SourcesManager.all_sets.map(&:specification)
-github_specs = specs.select(&:github?)
+specs = specs.first(10)
+
+github_specs = specs.select do |spec|
+  puts "Checking #{spec.name}"
+  sleep 1 # Be nice to GitHub
+  begin
+    spec.github? && !!spec.github_repo
+  rescue Octokit::NotFound
+    false
+  end
+end
+github_specs = github_specs.sort_by { |spec| - spec.github_repo.stargazers_count }
+
+
+github_specs.each do |spec|
+  puts "#{spec.name}: #{spec.github_repo.stargazers_count}"
+end
+
+# stargazers_count
+# forks_count
 
 binding.pry
-
